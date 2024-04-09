45598cabaa88a834d457cbedfaea9e52757280ac
Purushothaman <purushothaman.srinivasan@inspirisys.com>
2024-04-09 14:41:11 +0530

    jenkins build push

---
+++ Gitpush.java
@@ -0,0 +1,7 @@
+public class Gitpush {
+    public static void main(String[] args) {
+        System.out.print("This project is build used ");
+
+System.out.print( " for git hub push");   
+    }
+}
