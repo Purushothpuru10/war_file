411609fde64915bdd3e9edde8ce5a36dd70b4d6d
Purushothpuru10 <90284343+Purushothpuru10@users.noreply.github.com>
2024-04-09 16:51:58 +0530

    Delete Gitpush.java

--- Gitpush.java
+++
@@ -1,7 +0,0 @@
-public class Gitpush {
-    public static void main(String[] args) {
-        System.out.print("This project is build used ");
-
-System.out.print( " for git hub push");   
-    }
-}
